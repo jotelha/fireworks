38cd8e8e0bb7daa7d9ce33ce4b858b5a44b239b5
Johannes Hörmann <johannes.hoermann@imtek.uni-freiburg.de>
2019-07-17 18:26:53 +0200

    Added GetFilesByQueryTask to filepad_tasks.py

--- fireworks/user_objects/firetasks/filepad_tasks.py
+++ fireworks/user_objects/firetasks/filepad_tasks.py
@@ -80,6 +80,66 @@ class GetFilesTask(FiretaskBase):
             with open(os.path.join(dest_dir, file_name), "w") as f:
                 f.write(file_contents.decode())
 
+class GetFilesByQueryTask(FiretaskBase):
+    """
+    A Firetask to fetch files from the filepad and write it to specified directory (current working
+    directory if not specified)
+
+    Required params:
+        - query ([str]): mongo db query identifying files to fetch
+
+    Optional params:
+        - sort_key (str): sort key, don't sort per default
+        - sort_direction (int): sort direction, default 'pymongo-DESCENDING'
+        - limit (int): maximum number of files to write, default: no limit
+        - filepad_file (str): path to the filepad db config file
+        - dest_dir (str): destination directory, default is the current working
+          directory.
+        - new_file_names ([str]): if provided, the retrieved files will be
+          renamed. Not recommended as order and number of queried files not fixed.
+    """
+    _fw_name = 'GetFilesByQueryTask'
+    required_params = ["query"]
+    optional_params = ["sort_key","sort_direction", "limit",
+        "filepad_file", "dest_dir", "new_file_names"]
+
+    def run_task(self, fw_spec):
+        import pymongo
+        from pprint import pprint
+        fpad = get_fpad(self.get("filepad_file", None))
+        dest_dir = self.get("dest_dir", os.path.abspath("."))
+        new_file_names = self.get("new_file_names", [])
+        query = self.get("query", {})
+        sort_key = self.get("query", None)
+        sort_direction = self.get("query", pymongo.DESCENDING)
+        limit = self.get("limit",None)
+
+        # convert nested dicts to MongoDB query, i.e.
+        # { 'metadata' : { 'key': 'value' } --> { 'metadata.key': 'value' }
+        # ATTENTION: this does not work for more sophisticated queries
+        # such as { 'identifier': { '$in': [ 'id1', 'id2' ] } }
+        # TODO: find proper query translation method
+        def nested2plain(d, prefix=''):
+            r = {}
+            if prefix is not '':
+                prefix = prefix + '.'
+            for k, v in d.items():
+                if type(v) is dict:
+                    r.update( nested2plain(v, prefix + k) )
+                else:
+                    r.update( {prefix + k: v} )
+            return r
+
+        query = nested2plain(query)
+        pprint(query)
+
+        l = fpad.get_file_by_query(query,sort_key,sort_direction)
+        for i, (file_contents, doc) in enumerate(l):
+            if isinstance(limit, int) and i >= limit:
+                break # maximum number of files reached
+            file_name = new_file_names[i] if new_file_names else doc["original_file_name"]
+            with open(os.path.join(dest_dir, file_name), "wb") as f:
+                f.write(file_contents)
 
 class DeleteFilesTask(FiretaskBase):
     """
